a0b6e0cbe480c88216dfcbee0d67836934e1ac04
Joel Boada Srv Worknet <jmboada30@gmail.com>
2021-10-07 11:55:59 -0500

    inicia el curso de router y modularizacion

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,8 +5,12 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -19,15 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'my-store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.name).toEqual('my-store');
+    expect(app.title).toEqual('my-store');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'my-store app is running!'
-    );
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-store app is running!');
   });
 });
