8d268e70eb28b06ed8206f5b57a52faf91b670ab
Joel Boada Srv Worknet <jmboada30@gmail.com>
2021-10-08 01:02:12 -0500

    hasta los params en url

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
-import { StoreService } from '../../services/store.service'
+import { StoreService } from '../../services/store.service';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user.model';
+import { CategoriesService } from '../../services/categories.service';
+import { Category } from '../../models/category.model';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
-
   activeMenu = false;
   counter = 0;
   profile: User | null = null;
+  categories: Category[] = [];
 
   constructor(
     private storeService: StoreService,
     private authService: AuthService,
-  ) { }
+    private categoriesSvc: CategoriesService
+  ) {}
 
   ngOnInit(): void {
-    this.storeService.myCart$.subscribe(products => {
+    this.storeService.myCart$.subscribe((products) => {
       this.counter = products.length;
     });
+    this.getAllCategories();
   }
 
   toggleMenu() {
@@ -31,10 +35,16 @@ export class NavComponent implements OnInit {
   }
 
   login() {
-    this.authService.loginAndGet('john@mail.com', 'changeme')
-    .subscribe(user => {
-      this.profile = user;
-    });
+    this.authService
+      .loginAndGet('john@mail.com', 'changeme')
+      .subscribe((user) => {
+        this.profile = user;
+      });
   }
 
+  getAllCategories() {
+    this.categoriesSvc
+      .getAll()
+      .subscribe((categories) => (this.categories = categories));
+  }
 }
